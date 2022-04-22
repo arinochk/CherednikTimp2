74377fa400ea97d6409809aadd195e3716607d33
arinacherednik <arina.cherednik@yandex.ru>
2022-04-22 19:34:07 +0300

    transtation

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-//Комментарий
-//Комментарий
+//Comment
+//Comment
 std::cout << "Пожалуйста, введите свое имя"<<std::endl;
 std::string name;
 std::cout<<" Hello world from "<< name<<std::endl;
